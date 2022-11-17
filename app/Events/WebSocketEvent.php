7b2ddddecf09878a7e42bab976abe6ab21404a45
Tuhin Rehan <tuhin.rehan@netmediamobile.com>
2022-11-17 23:51:15 +0600

    Event created for broadcasting

---
+++ app/Events/WebSocketEvent.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PublicChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\User;
+
+class WebSocketEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    protected $users_data;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->users_data = User::all();
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PublicChannel('channel-name');
+    }
+}
